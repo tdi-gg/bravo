15a8ee3917c99c548e3d83b435cb5178df22884c
Kentaro Yamauchi <YAMAUCHI-Kentarou@tdi.co.jp>
2018-09-19 09:42:59 +0900

    スピーカー情報の全件取得機能を修正。

--- BravoForIotc/js/bravo3.js
+++ BravoForIotc/js/bravo3.js
@@ -104,7 +104,7 @@ var showSpeakerSelect = function() {
 		console.log("speakers", speakers);
 		var speakerSelect = $("ul.menu")[0];
 		for (var i = 0; i < speakers.length; i++) {
-			speakerSelect.append("<li>" + speakers.SPEAKER_NAME + "</li>");
+			$(speakerSelect).append("<li>" + speakers.SPEAKER_NAME + "</li>");
 		}
 	});
 	
